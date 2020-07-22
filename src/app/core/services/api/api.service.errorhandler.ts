18ab6f578f5c986500cad338d4abe24079f18f36
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-07-22 18:44:54 +0200

    fixup! fixup! refactor: push http error mapping from mapErrorToAction to ApiServiceErrorHandler

--- src/app/core/services/api/api.service.errorhandler.ts
+++ src/app/core/services/api/api.service.errorhandler.ts
@@ -1,8 +1,8 @@
 import { isPlatformBrowser } from '@angular/common';
-import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorHandler, Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { EMPTY, Observable, of, throwError } from 'rxjs';
+import { EMPTY, MonoTypeOperatorFunction, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { HttpErrorMapper } from 'ish-core/models/http-error/http-error.mapper';
 import { communicationTimeoutError, serverError } from 'ish-core/store/core/error';
@@ -15,27 +15,23 @@ export class ApiServiceErrorHandler {
     @Inject(PLATFORM_ID) private platformId: string
   ) {}
 
-  // tslint:disable-next-line:ban-types
-  dispatchCommunicationErrors<T>(error: HttpErrorResponse): Observable<T> {
-    if (!isPlatformBrowser(this.platformId)) {
-      this.errorHandler.handleError(error);
-    }
-    const mappedError = HttpErrorMapper.fromError(error);
+  handleErrors<T>(dispatch: boolean): MonoTypeOperatorFunction<T> {
+    return catchError(error => {
+      if (!isPlatformBrowser(this.platformId)) {
+        this.errorHandler.handleError(error);
+      }
+      const mappedError = HttpErrorMapper.fromError(error);
 
-    if (error.status === 0) {
-      this.store.dispatch(communicationTimeoutError({ error: mappedError }));
-      return EMPTY;
-    }
-    if (error.status >= 500 && error.status < 600) {
-      this.store.dispatch(serverError({ error: mappedError }));
-      return EMPTY;
-    }
-    return throwError(mappedError);
-  }
-
-  // tslint:disable-next-line: ban-types
-  mapError<T>(error: HttpErrorResponse): Observable<T> {
-    // re-cast for easier access
-    return (of(HttpErrorMapper.fromError(error)) as unknown) as Observable<T>;
+      if (dispatch) {
+        if (error.status === 0) {
+          this.store.dispatch(communicationTimeoutError({ error: mappedError }));
+          return EMPTY;
+        } else if (error.status >= 500 && error.status < 600) {
+          this.store.dispatch(serverError({ error: mappedError }));
+          return EMPTY;
+        }
+      }
+      return throwError(mappedError);
+    });
   }
 }
